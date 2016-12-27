8ba6df0e1dc1808d40c9261c5ada54a5254688f3
James Kuszlewicz <jsk389@bham.ac.uk>
2016-12-27 20:58:49 +0000

    Tidied up plotting

--- Code/hierarch-inference/check_model_loc.py
+++ Code/hierarch-inference/check_model_loc.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-
+# -*- coding: utf-8 -*-
 from __future__ import division
 from __future__ import print_function
 
-import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
@@ -81,40 +81,43 @@ if __name__=="__main__":
     cos_fish = np.zeros([N, len(kappa)])
     color=['cyan', 'blue', 'green', 'red', 'magenta', 'orange', 'saddlebrown',
            'olive', 'gold']
+
+    # Plot in inc
     for i in range(len(kappa)):
         for j in range(len(mu)):
-
             fish[:,i] = inv_sampl(angles, model(angles, [kappa[i], mu[j]]), N)
-            pl.plot(angles, model(angles, [kappa[i], mu[j]]))
-            pl.hist(fish[:,i], bins=np.sqrt(N), normed=True, \
-                    histtype='step', color=color[i], linewidth=2, \
-                    label=r'$\kappa$ = '+str(kappa[i])+'; $\mu$ = '+str(mu[j]))
-            pl.show()
-            cos_fish[:,i] = np.cos(fish[:,i])
-            pl.plot(np.cos(angles), cosine_fisher_pdf(np.cos(angles), [kappa[i], mu[j]]))
-            pl.hist(cos_fish[:,i], bins=np.sqrt(N), normed=True, \
-                    histtype='step', color=color[i], linewidth=2, \
-                    label=r'$\kappa$ = '+str(kappa[i])+'; $\mu$ = '+str(mu[j]))
-            #y = np.cos(np.radians(angles))
-            #pl.plot(y, cos_fisher(y, kappa[i]), linestyle='-', \
-            #        linewidth=2, color=color[i], label=r'$\kappa$ = '+str(kappa[i]))
-            pl.show()
-    pl.ylabel(r'Probability Density', fontsize=18)
-    pl.xlabel(r'$\cos i$', fontsize=18)
-    pl.legend(loc='best')
-    pl.show()
+            plt.plot(angles, model(angles, [kappa[i], mu[j]]), color=color[i])
+            if mu[j] == np.pi/2.0:
+                plt.hist(fish[:,i], bins=np.sqrt(N), normed=True, \
+                        histtype='step', color=color[i], linewidth=2, \
+                        label=r'$\kappa$ = '+str(kappa[i])+'; $\mu$ = $\pi$/2')
+            else:
+                plt.hist(fish[:,i], bins=np.sqrt(N), normed=True, \
+                        histtype='step', color=color[i], linewidth=2, \
+                        label=r'$\kappa$ = '+str(kappa[i])+'; $\mu$ = '+str(mu[j]))
+    plt.xlim(0, np.pi/2.0)
+    plt.ylabel(r'Probability Density', fontsize=18)
+    plt.xlabel(r'$i_{\mathrm{s}}$ (radians)', fontsize=18)
+    plt.legend(loc='best')
+    plt.savefig('fisher_loc_inc.png')
+    plt.show()
 
-    #kappa = [0.0, 0.5, 1.0, 2.0, 3.0, 4.0]
     for i in range(len(kappa)):
-        fish[:,i] = inv_sampl(angles, fisher(angles, kappa[i]), N)
-        #pl.hist(fish[:,i], bins=np.sqrt(N), normed=True, \
-        #        histtype='step', color=color[i], linewidth=2, \
-        #        label=r'$\kappa$ = '+str(kappa[i]))
-        y = angles
-        pl.plot(y, fisher(y, kappa[i]), linestyle='-', \
-                linewidth=2, color=color[i], label=r'$\kappa$ = '+str(kappa[i]))
-    pl.ylabel(r'Probability Density', fontsize=18)
-    pl.xlabel(r'$i$', fontsize=18)
-    pl.ylim([0,0.03])
-    pl.legend(loc='best')
-    pl.show()
+        for j in range(len(mu)):
+            fish[:,i] = inv_sampl(angles, model(angles, [kappa[i], mu[j]]), N)
+            cos_fish[:,i] = np.cos(fish[:,i])
+            plt.plot(np.cos(angles), cosine_fisher_pdf(np.cos(angles), [kappa[i], mu[j]]), color=color[i])
+            if mu[j] == np.pi/2.0:
+                plt.hist(cos_fish[:,i], bins=np.sqrt(N), normed=True, \
+                        histtype='step', color=color[i], linewidth=2, \
+                        label=r'$\kappa$ = '+str(kappa[i])+'; $\mu$ = $\pi$/2')
+            else:
+                plt.hist(cos_fish[:,i], bins=np.sqrt(N), normed=True, \
+                        histtype='step', color=color[i], linewidth=2, \
+                        label=r'$\kappa$ = '+str(kappa[i])+'; $\mu$ = '+str(mu[j]))
+    plt.xlim(0, 1.0)
+    plt.ylabel(r'Probability Density', fontsize=18)
+    plt.xlabel(r'$\cos i_{\mathrm{s}}$', fontsize=18)
+    plt.legend(loc='best')
+    plt.savefig('fisher_loc_cosine_inc.png')
+    plt.show()
